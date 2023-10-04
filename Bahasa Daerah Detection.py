8b7c26c13fa5ba61e3300fdc46a444c6a9e40e00
bimarakajati <bimandugal@gmail.com>
2023-10-04 21:43:40 +0700

    add gitignore

---
+++ Bahasa Daerah Detection.py
@@ -0,0 +1,25 @@
+import pickle
+
+# Load the pickled model
+with open('model/lrmodel.pckl', 'rb') as model_file:
+    loaded_model = pickle.load(model_file)
+
+while True:
+    # Prepare your input data
+    input_text = input("Enter your text: ")
+
+    # Make predictions using the loaded model
+    predicted_language = loaded_model.predict([input_text])
+
+    # Get the class probabilities as confidence scores
+    class_probabilities = loaded_model.predict_proba([input_text])
+
+    # The variable 'predicted_language' now contains the predicted language or value
+    print("Predicted language:", predicted_language[0])
+
+    # The variable 'class_probabilities' contains the class probabilities
+    print("Class Probabilities:", class_probabilities[0])
+
+    # If you want to get the confidence score for the predicted class (the max probability)
+    confidence_score = max(class_probabilities[0])
+    print("Confidence Score:", confidence_score)
---
+++ README.md
@@ -0,0 +1,15 @@
+## 🔮 Future Improvements:
+- [x] Adding/repairing the dataset
+- [x] Increase model accuracy
+- [ ] Deploying the model
+- [ ] Add new language
+
+## 📝 Note:
+    Feel free to fork this repository, modify the code, and create a pull request. I will be happy to review and merge it.
+
+    If you have any questions, please feel free to contact me. Happy scraping! 🕷️
+
+## 📙 Reference:
+- https://github.com/akhiilkasare/Language-Detection-Using-NLP-and-Machine-Learning
+- https://github.com/IndoNLP/nusax
+- https://github.com/Wikidepia/indonesian_datasets
