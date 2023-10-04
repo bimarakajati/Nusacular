191e8b3ed66bd2334779b7eb5df7c53ab51bc50b
bimarakajati <bimandugal@gmail.com>
2023-10-04 08:48:17 +0700

    new branch and minor fix

--- Bahasa Daerah Detection.py
+++ Bahasa Daerah Detection.py
@@ -4,11 +4,12 @@ import pickle
 with open('lrmodel.pckl', 'rb') as model_file:
     loaded_model = pickle.load(model_file)
 
-# Prepare your input data
-input_text = input("Enter your text: ")
+while True:
+    # Prepare your input data
+    input_text = input("Enter your text: ")
 
-# Make predictions using the loaded model
-predicted_class = loaded_model.predict([input_text])
+    # Make predictions using the loaded model
+    predicted_class = loaded_model.predict([input_text])
 
-# The variable 'predicted_class' now contains the predicted class or value
-print("Predicted class:", predicted_class)
+    # The variable 'predicted_class' now contains the predicted class or value
+    print("Predicted class:", predicted_class)
