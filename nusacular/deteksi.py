be9a188dd85a9cb812f660fa39b6a76fb0fc6a23
bimarakajati <bimandugal@gmail.com>
2023-10-23 08:21:56 +0700

    translate

--- nusacular/deteksi.py
+++ nusacular/deteksi.py
@@ -17,25 +17,33 @@ def deteksi():
     st.title("Deteksi Bahasa Daerah")
 
     input_kata = st.text_input(label="Masukkan kalimat:")
-    prediksi = st.button ("Deteksi")
+    prediksi = st.button("Deteksi")
 
     if prediksi and input_kata != "" or input_kata:
         input_kata = preprocessing(input_kata)
         predicted_language = loaded_model.predict([input_kata])
         print("Predicted language:", predicted_language)
-        st.write("Predicted Language:", predicted_language[0])
+        if predicted_language[0] == "bataknese":
+            predicted_language[0] = "Batak Toba"
+        elif predicted_language[0] == "indonesian":
+            predicted_language[0] = "Indonesia"
+        elif predicted_language[0] == "javanese":
+            predicted_language[0] = "Jawa"
+        elif predicted_language[0] == "sundanese":
+            predicted_language[0] = "Sunda"
+        st.write("Prediksi bahasa:", predicted_language[0])
 
         class_probabilities = loaded_model.predict_proba([input_kata])
         print("Class probabilities:", class_probabilities)
 
         confidence_score = max(class_probabilities[0])
-        st.write("Confidence Score:", confidence_score)
+        st.write(f"Confidence score: `{round(confidence_score * 100, 2)}%`")
 
         result = [
-            ["Bataknese", class_probabilities[0][0]],
-            ["Indonesian", class_probabilities[0][1]],
-            ["Javanese", class_probabilities[0][2]],
-            ["Sundanese", class_probabilities[0][3]]
+            ["Batak Toba", class_probabilities[0][0]],
+            ["Indonesia", class_probabilities[0][1]],
+            ["Jawa", class_probabilities[0][2]],
+            ["Sunda", class_probabilities[0][3]]
         ]
         result.sort(key=lambda x: x[1], reverse=True)
 
