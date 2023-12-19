f88481f726ff868242a37f657ecf083a40e802b7
bimarakajati <bimandugal@gmail.com>
2023-12-19 21:05:24 +0700

    major updates

---
+++ nusacular/about.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+def about_us():
+    st.title("About Us")
+
+    # About Us
+    st.markdown("""
+    Dibuat pada 4 Oktober 2023, Nusacular (Nusantara Vernacular) adalah sebuah aplikasi yang dapat mendeteksi atau menerjemahkan bahasa daerah, mengubah teks menjadi suara, sentimen analisis dan juga dapat digunakan sebagai chatbot.
+
+    Bahasa daerah yang dapat diprediksi oleh aplikasi ini adalah:
+    - Bahasa Aceh
+    - Bahasa Bali
+    - Bahasa Banjar (Kalimantan Selatan)
+    - Bahasa Bugis (Sulawesi Selatan)
+    - Bahasa Jawa
+    - Bahasa Madura
+    - Bahasa Minang (Sumatera Utara)
+    - Bahasa Ngaju (Dayak)
+    - Bahasa Sunda
+    - Bahasa Batak (Toba)
+
+    Aplikasi ini dibuat oleh:
+    |             Name            |      NIM       |
+    | --------------------------- | -------------- |
+    | Bima Rakajati               | A11.2020.13088 |
+    | Enrico Zada                 | A11.2020.12972 |
+    | Rosalia Natal Silalahi      | A11.2020.13084 |
+    | Devi Kartika Sari           | A11.2020.12518 |
+    """)
---
+++ nusacular/suara.py
@@ -0,0 +1,25 @@
+from gtts import gTTS
+import streamlit as st
+
+def text2speech():
+    st.title("Suara Bahasa Daerah")
+
+    language = st.selectbox(
+    'Pilih bahasa daerah:',
+    ('Jawa', 'Sunda'))
+    
+    if language == 'Jawa':
+        bahasa = "jw"
+    elif language == 'Sunda':
+        bahasa = "su"
+
+    teks_asal = st.text_input(label="Masukkan kalimat:")
+    prediksi = st.button ("Suarakan")
+
+    if prediksi:
+        suara = gTTS(text=teks_asal, lang=bahasa, slow=False)
+        # Simpan suara ke dalam file
+        suara.save("output.mp3")
+
+        st.write("Hasil:")
+        st.audio("output.mp3")
