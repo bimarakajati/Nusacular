2612cc4c9d80591c48999f181b49b97eb053f5bc
bimarakajati <bimandugal@gmail.com>
2023-12-19 21:04:25 +0700

    major updates

--- chatbot/bard.py
+++
@@ -1,5 +0,0 @@
-from bardapi import Bard
-
-token = 'bgjOeEh9zBaXR25UIbLMtUTKbD9E9PLVewpXiQLIIPDFrZpErWxxjSpgOeMePmoQs1Rnyw.'
-bard = Bard(token=token)
-bard.get_answer("apa itu bahasa daerah?")['content']
--- chatbot/bard_autocookies.py
+++
@@ -1,5 +0,0 @@
-from bardapi import Bard
-
-bard = Bard(token_from_browser=True)
-res = bard.get_answer("Do you like cookies?")
-print(res['content'])
